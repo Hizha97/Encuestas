40d8b8c8cee851699f289790c6245e6a41afbc8f
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2017-11-05 13:30:16 +0100

    Refactoring and cleaning index.

---
+++ Pages/AbstractPage.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antonio
+ * Date: 05/11/2017
+ * Time: 13:10
+ */
+
+require_once("RenderTrait.php");
+
+abstract class AbstractPage
+{
+    use RenderTrait;
+
+    abstract public function head();
+    abstract public function beforeBodyScripts();
+    abstract public function body();
+    abstract public function afterBodyScripts();
+
+    public function render()
+    {
+        echo '<!DOCTYPE html>';
+        echo '<html>';
+        echo '<head>';
+        $this->head();
+        echo '</head>';
+        echo '<script>';
+        $this->beforeBodyScripts();
+        echo '</script>';
+        echo '<body>';
+        $this->body();
+        echo '</body>';
+        echo '<script>';
+        $this->afterBodyScripts();
+        echo '</script>';
+        echo '</html>';
+    }
+}
+
---
+++ Pages/pages.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antonio
+ * Date: 05/11/2017
+ * Time: 13:13
+ */
+
+require_once ('AbstractPage.php');
