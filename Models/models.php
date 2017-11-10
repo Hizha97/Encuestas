d4253f6afda553191c80fecf82f83bf72f024202
Hizha97 <hizha97@gmail.com>
2017-11-10 20:52:10 +0100

    VistaDetallePregunta

--- Models/models.php
+++ Models/models.php
@@ -11,3 +11,6 @@ require_once('ExampleModel.php');
 require_once('Foo/Usuario.php');
 require_once('Foo/TitulacionAsignaturaProfesor.php');
 require_once('Foo/Pregunta.php');
+require_once('Foo/Navbar.php');
+require_once('Foo/Seccion.php');
+require_once('Foo/Tipo.php');
